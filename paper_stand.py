84b14a778621b4f9ff9cabde3499f6ec2e2652ab
Raghuttam Hombal <group4pgs@gmail.com>
2023-07-23 22:51:41 +0530

    Argument Parser

--- paper_stand.py
+++ paper_stand.py
@@ -1,13 +1,46 @@
 import json
 import sys
+import os
+import getopt
 
-dbf = open('db.json')
-db = json.loads(dbf.read())
 
-search_tag = sys.argv[1]
-for i in db:
-    if search_tag in i['tags']:
-        print('Name -> ',i['name'])
-        print('Path -> ',i['loc'])
+def search_by_tags(tags):
+    dbf = open('db.json')
+    db = json.loads(dbf.read())
 
-dbf.close()
+    search_tag = tags
+    for i in db:
+        if search_tag in i['tags']:
+            print('Name \t\t-> ',i['name'])
+            print('Path \t\t-> ',i['loc'])
+            print('Other tags\t -> ',i['tags'])
+
+    dbf.close()
+
+
+
+
+if __name__=='__main__':
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h:s:n:u:t:a", ["help","search","name","doi","tags","add"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    #print(opts)
+
+    #print("Using option %s"%args[0][0])
+    tags=[]
+    for o,a in opts:
+        if o in ('-t','--tags'):
+            tags = a
+
+    for o,a in opts:
+        if o in ('--search','-s'):
+            search_by_tags(tags)
+        if o in ('--help','-h'):
+            usage()
+
+
+def usage():
+    print('Instruction to use')
