22a8a3d4222d0ab3062c9e9c45130a45e513990e
Raghuttam Hombal <group4pgs@gmail.com>
2023-07-23 18:38:25 +0530

    Initial Commit: Trying to make an arch

---
+++ README.md
@@ -0,0 +1,1 @@
+A tool to manage a number of research papers in your PC
--- paper_stand.py
+++ paper_stand.py
@@ -1,1 +1,13 @@
 import json
+import sys
+
+dbf = open('db.json')
+db = json.loads(dbf.read())
+
+search_tag = sys.argv[1]
+for i in db:
+    if search_tag in i['tags']:
+        print('Name -> ',i['name'])
+        print('Path -> ',i['loc'])
+
+dbf.close()
