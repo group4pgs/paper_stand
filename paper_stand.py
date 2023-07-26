07ceacc16e65f297fc9d7726037f049d56a25ea7
Raghuttam Hombal <group4pgs@gmail.com>
2023-07-26 22:11:08 +0530

    Feature; adding paper v0.1

--- paper_stand.py
+++ paper_stand.py
@@ -5,7 +5,7 @@ import getopt
 
 
 def search_by_tags(tags):
-    dbf = open('db.json')
+    dbf = open('db.json','r+')
     db = json.loads(dbf.read())
 
     search_tag = tags
@@ -17,7 +17,31 @@ def search_by_tags(tags):
 
     dbf.close()
 
+def add_paper():
+    paper_name = input("Name \t->")
+    paper_doi = input("DOI \t->")
+    paper_loc = input("Path \t->")
+    paper_tags = input("Tags \t->").split(',')
 
+    db = ""
+    with open('db.json','r') as db_file:
+        db = json.load(db_file)
+        #print (db)
+
+    new_entry = {"name":paper_name,"doi":paper_doi,"loc":paper_loc,"tags":paper_tags}
+
+    db.append(new_entry)
+
+    with open('db.json','w') as db_file:
+        json.dump(db,db_file)
+
+
+    return True
+
+
+def usage():
+    print('Instruction to use')
+    
 
 
 if __name__=='__main__':
@@ -40,7 +64,10 @@ if __name__=='__main__':
             search_by_tags(tags)
         if o in ('--help','-h'):
             usage()
+        if o in ('--add','-a'):
+            if add_paper():
+                print("Paper successfully added!")
+            else:
+                print("ERROR: Problem adding the paper, Please try again or check the 'db.json' file!")
 
 
-def usage():
-    print('Instruction to use')
